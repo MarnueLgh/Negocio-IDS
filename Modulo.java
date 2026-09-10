c2c24168fc480289652b0274a0cefb7a5ecda579
Saul Alvarez <saul_alvarez_aguilar@comunidad.unam.mx>
2026-09-10 08:09:02 -0600

    fix: Clase Modulo.java Final Final NO TOCAR

--- Modulo.java
+++ Modulo.java
@@ -3,6 +3,6 @@ public class Modulo {
     public double calcular(double a, double b) {
         return a % b;
 
-        /**Hola, mensaje de prueba */
+        //Hola Maestra, pongame diez porfavor <3
     }
 }
